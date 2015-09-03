8e4736fefa0cadac97ec1ccb67e99a0b56c683dd
tjbp <tom@tjbp.net>
2015-09-03 17:13:04 +0100

    Added getAll() functionality to HasCurrencies

--- HasCurrencies.php
+++ HasCurrencies.php
@@ -13,11 +13,15 @@ trait HasCurrencies
      * @param mixed $code
      * @return array
      */
-    private function currency($code)
+    private function currency($code = null)
     {
         // Store the object to prevent unnecessary instantiation
         if (is_null($this->iso4217)) {
             $this->iso4217 = new ISO4217;
+        }
+
+        if (is_null($code)) {
+            $this->iso4217->getAll();
         }
 
         return $this->iso4217->getByCode($code);
